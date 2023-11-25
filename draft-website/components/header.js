6d9794c324db0586344c2e9423625738d16db4d4
parth448812 <parthkpatel04@gmail.com>
2023-11-24 19:01:25 -0500

    Site-wide Major CSS/Site Creation Update
    Creation of Signed-In page, Game Prep Page, and End Game page.
    
    Serious updates to main page, game page, login page, and page page.

--- draft-website/components/header.js
+++ draft-website/components/header.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
         <h1>NinerNav</h1>
         <nav>
         <a href="main.html">Home Page</a> |
+        <a href="signed-in.html">Signed-In</a> |
         <a href="login.html">Login</a> |
+        <a href="gameprep.html">Game Prep</a> |
         <a href="gamepage.html">Game Page</a> |
         <a href="leaderboard.html">Leaderboard</a> |
-        <a href="resultpage.html">Result Page</a>
+        <a href="resultpage.html">Result Page</a> |
+        <a href="end-game.html">End Game Page</a>
       </nav>
     </div>
       <br>
