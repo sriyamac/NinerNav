30c7fe2bce26f6c329e3af9fbde18c4fc11ede17
Manny M <89672389+G00NSIE@users.noreply.github.com>
2023-12-07 13:08:45 -0500

    iframe for maps on results page
    dosnt work yet need DB and flask integration

---
+++ static/components/result.js
@@ -0,0 +1,22 @@
+let map;
+
+let marker;
+
+//postion var needs to be connected with the proper coords from the DB
+//right now they are hard coded
+let position = {lat: 35.308168, lng: -80.733699}
+
+function initMap() {
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: position,
+        zoom: 17
+    });
+
+    marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+    
+    map.panTo(position);
+}
